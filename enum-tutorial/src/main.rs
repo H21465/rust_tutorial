01a9eca4e21e7e33fc54a984ff51ac8ebe9a158c
H21465 <yamamotohayato1273@icloud.com>
2025-08-17 16:02:26 +0900

    [add] enum and my-project

---
+++ enum-tutorial/src/main.rs
@@ -0,0 +1,45 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let penny = Coin::Penny;
+    let alaska_quarter = Coin::Quarter(UsState::Alaska);
+    println!("A penny is worth {} cents(s).", value_in_cents(penny));
+    println!("A quarter is worth {} cents(s).", value_in_cents(alaska_quarter));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("six {}", six);
+}
